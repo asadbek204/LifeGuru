f62c1e760489a70c9c609dcaa8235327b0b94d54
Asadbek <ahikmatullayev024@gmail.com>
2024-06-03 08:49:02 +0500

    adding dist

--- src/contexts.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react"
-import { TGlobalContext, THomeContext } from "./types"
-
-const GlobalContext = createContext<TGlobalContext>({ })
-
-export const HomeContext = createContext<THomeContext>({ })
-
-export default GlobalContext
